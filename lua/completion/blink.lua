24ca56d813c21effd248957d5b88114076f12385
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-04 10:11:19 +0800

    feat(blink): add conventional commit and tweak keymap

--- lua/completion/blink.lua
+++ lua/completion/blink.lua
@@ -1,13 +1,12 @@
 return {
   {
-    "blink.cmp.conventional.commits",
+    "blink-cmp-conventional-commits",
     dep_of = { "blink.cmp" },
   },
   {
     "colorful-menu.nvim",
     on_plugin = { "blink.cmp" },
   },
-  -- FIXME: Revisit if cmp_cmdline is worth using
   {
     "blink.cmp",
     event = "DeferredUIEnter",
@@ -17,35 +16,36 @@ return {
         -- Also see ./luasnip.lua for additional keys, like node choices
         keymap = {
           -- FIXME: unset once done with the below
-          preset = 'default',
+          preset = "default",
 
           -- These are the defaults per: https://cmp.saghen.dev/configuration/keymap.html
 
-          ['<C-space>'] = false,
-          ['<Up>'] = false,
-          ['<Down>'] = false,
-          ['<Tab>'] = false,
-          ['<S-Tab>'] = false,
+          ["<C-space>"] = false,
+          ["<Up>"] = false,
+          ["<Down>"] = false,
+          ["<Tab>"] = false,
+          ["<S-Tab>"] = false,
 
-          ['<C-s>'] = { 'show', 'show_documentation', 'hide_documentation', 'fallback' },
-          ['<C-t>'] = { 'show_signature', 'hide_signature', 'fallback' },
+          ["<C-s>"] = { "show", "show_documentation", "hide_documentation", "fallback" },
+          ["<C-t>"] = { "show_signature", "hide_signature", "fallback" },
 
           -- NOTE: c-e has some odd behavior, so is annoying if you over press
-          ['<C-e>'] = { 'hide', 'hide_documentation', 'hide_signature', }, --'fallback' },
-          ['<C-y>'] = { 'select_and_accept', 'fallback' },
+          ["<C-e>"] = { "hide", "hide_documentation", "hide_signature" }, --'fallback' },
+          ["<C-y>"] = { "select_and_accept", "fallback" },
 
-          ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
-          ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
+          -- FIXME: Switch n/p to j/k but figure out why cmdline completion is using n/p still
+          ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
+          ["<C-j>"] = { "select_next", "fallback_to_mappings" },
 
-          ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-          ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+          ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+          ["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-          ['<C-n>'] = { 'snippet_forward', 'fallback' },
-          ['<C-p>'] = { 'snippet_backward', 'fallback' },
-
+          ["<C-n>"] = { "snippet_forward", "fallback" },
+          ["<C-p>"] = { "snippet_backward", "fallback" },
         },
         cmdline = {
           enabled = true,
+          keymap = { preset = "inherit" },
           completion = {
             menu = {
               auto_show = true,
@@ -54,18 +54,22 @@ return {
           sources = function()
             local type = vim.fn.getcmdtype()
             -- Search forward and backward
-            if type == '/' or type == '?' then return { 'buffer' } end
+            if type == "/" or type == "?" then
+              return { "buffer" }
+            end
             -- Commands
-            if type == ':' or type == '@' then return { 'cmdline', } end
+            if type == ":" or type == "@" then
+              return { "cmdline" }
+            end
             return {}
           end,
         },
         fuzzy = {
           sorts = {
-            'exact',
+            "exact",
             -- defaults
-            'score',
-            'sort_text',
+            "score",
+            "sort_text",
           },
         },
         signature = {
@@ -77,7 +81,7 @@ return {
         completion = {
           menu = {
             draw = {
-              treesitter = { 'lsp' },
+              treesitter = { "lsp" },
               components = {
                 label = {
                   text = function(ctx)
@@ -95,20 +99,22 @@ return {
           },
         },
         snippets = {
-          preset = 'luasnip',
+          preset = "luasnip",
           active = function(filter)
             local snippet = require("luasnip")
             local blink = require("blink.cmp")
             if snippet.in_snippet() and not blink.is_visible() then
               return true
             else
-              if not snippet.in_snippet() and vim.fn.mode() == "n" then snippet.unlink_current() end
+              if not snippet.in_snippet() and vim.fn.mode() == "n" then
+                snippet.unlink_current()
+              end
               return false
             end
           end,
         },
         sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer', 'omni' },
+          default = { "conventional_commits", "lsp", "path", "snippets", "buffer", "omni" },
           -- FIXME: Revisit these values
           providers = {
             path = {
@@ -123,9 +129,22 @@ return {
             cmdline = {
               score_offset = -100,
             },
+            -- FIXME: Make sure this is actually needed
+              conventional_commits = {
+                name = "Conventional Commits",
+                module = "blink-cmp-conventional-commits",
+                enabled = function()
+                  return vim.bo.filetype == "gitcommit"
+                end,
+                ---@module 'blink-cmp-conventional-commits'
+                ---@type blink-cmp-conventional-commits.Options
+                opts = {
+                  -- See Configuration section below for available options
+                },
+              },
           },
         },
       })
     end,
-  }
+  },
 }
