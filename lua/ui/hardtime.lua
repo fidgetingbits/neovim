bcc614ec00248b560815683eb5dffabb874c5a36
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-05 08:40:56 +0800

    fix(hardtime): remove arrow keys, since I use a meta+hjkl for some stuff

--- lua/ui/hardtime.lua
+++ lua/ui/hardtime.lua
@@ -3,7 +3,15 @@ return {
     'hardtime.nvim',
     event = 'DeferredUIEnter',
     after = function(plugin)
-      require('hardtime').setup({})
+      require('hardtime').setup({
+        disabled_keys = {
+          -- Allows mouse scroll and I use meta+hjkl for movement in insert
+          ['<Up>'] = false,
+          ['<Down>'] = false,
+          ['<Left>'] = false,
+          ['<Right>'] = false,
+        },
+      })
     end,
   },
 }
