bd2ea93c1e92500071c5ee6d60d5227ccd77fe16
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-06 07:46:09 +0800

    style(noice): linting and switch back to non-popup for awhile

--- lua/ui/noice.lua
+++ lua/ui/noice.lua
@@ -2,11 +2,11 @@ return {
   {
     'noice.nvim',
     event = 'DeferredUIEnter',
-    after = function(plugin)
+    after = function(_)
       require('noice').setup({
         cmdline = {
-          -- view = "cmdline",
-          view = 'cmdline_popup',
+          view = 'cmdline',
+          -- view = 'cmdline_popup',
         },
         lsp = {
           override = {
@@ -19,7 +19,7 @@ return {
         },
 
         presets = {
-          bottom_search = false, -- use a classic bottom cmdline for search
+          bottom_search = true, -- use a classic bottom cmdline for search
           command_palette = false, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
