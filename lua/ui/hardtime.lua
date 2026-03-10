b5412e118c26dfee40529c6e58d0ebaae34fd15d
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-10 17:06:14 +0800

    fix(hardtime): disable hjkl repeat warning

--- lua/ui/hardtime.lua
+++ lua/ui/hardtime.lua
@@ -4,6 +4,15 @@ return {
     event = 'DeferredUIEnter',
     after = function(plugin)
       require('hardtime').setup({
+        -- I too often scroll code while drinking, etc
+        restricted_keys = {
+          ['h'] = false,
+          ['j'] = false,
+          ['k'] = false,
+          ['l'] = false,
+          ['gj'] = false,
+          ['gk'] = false,
+        },
         disabled_keys = {
           -- Allows mouse scroll and I use meta+hjkl for movement in insert
           ['<Up>'] = false,
