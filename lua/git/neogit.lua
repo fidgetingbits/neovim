ea19c5437bd7148dfb434cc8f5631355bdeafdd8
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-14 17:48:00 +0800

    fix(neogit): typo in cmd

--- lua/git/neogit.lua
+++ lua/git/neogit.lua
@@ -2,9 +2,8 @@ return {
   {
     'neogit',
     event = 'DeferredUIEnter',
-    -- stylua: ignore
     keys = {
-      { '<leader>gg', vim.cmd.NeoGit, mode = { 'n' }, noremap = true, desc = 'Toggle neogit buffer', },
+      { '<leader>gg', vim.cmd.Neogit, mode = { 'n' }, desc = 'Toggle neogit buffer' },
     },
     after = function(plugin)
       require('neogit').setup({})
