344e9b6a1de006eac93e8eb9fb3d316d5ae1d056
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-16 11:12:13 +0800

    style(lualine): switch to short mode indicator

--- lua/ui/lualine.lua
+++ lua/ui/lualine.lua
@@ -1,3 +1,31 @@
+local function mode()
+  -- FIXME: Tweak these to differentiate the mode
+  -- Map of modes to their respective shorthand indicators
+  local mode_map = {
+    n = 'N', -- Normal mode
+    i = 'I', -- Insert mode
+    v = 'V', -- Visual mode
+    [''] = 'VB', -- Visual block mode
+    V = 'VL', -- Visual line mode
+    c = 'C', -- Command-line mode
+    no = 'N', -- NInsert mode
+    s = 'S', -- Select mode
+    S = 'SL', -- Select line mode
+    ic = 'IC', -- Insert mode (completion)
+    R = 'R', -- Replace mode
+    Rv = 'RV', -- Virtual Replace mode
+    cv = 'C', -- Command-line mode
+    ce = 'CE', -- Ex mode
+    r = 'P', -- Prompt mode
+    rm = 'M', -- More mode
+    ['r?'] = '?', -- Confirm mode
+    ['!'] = '!', -- Shell mode
+    t = 'T', -- Terminal mode
+  }
+  -- Return the mode shorthand or [UNKNOWN] if no match
+  return mode_map[vim.fn.mode()] or '[UNKNOWN]'
+end
+
 return {
   {
     'lualine.nvim',
@@ -10,6 +38,7 @@ return {
           section_separators = '',
         },
         sections = {
+          lualine_a = { { mode } },
           lualine_c = {
             {
               function()
