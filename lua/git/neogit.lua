39d5884a086675f9a9bc7cc9a7c60859de3eb6db
fidgetingbits <fidgetingbits@users.noreply.github.com>
2026-03-23 15:00:05 +0800

    refactor(neogit): default to split for neogit

--- lua/git/neogit.lua
+++ lua/git/neogit.lua
@@ -3,7 +3,15 @@ return {
     'neogit',
     event = 'DeferredUIEnter',
     keys = {
-      { '<leader>gg', vim.cmd.Neogit, mode = { 'n' }, desc = 'Toggle neogit buffer' },
+      { '<leader>gG', vim.cmd.Neogit, mode = { 'n' }, desc = 'Toggle neogit buffer' },
+      {
+        '<leader>gg',
+        function()
+          require('neogit').open({ kind = 'split' })
+        end,
+        mode = { 'n', 'v' },
+        desc = 'Open neogit in split',
+      },
     },
     after = function(plugin)
       require('neogit').setup({})
